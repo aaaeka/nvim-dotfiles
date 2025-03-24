4e091d864345882bf6a55151d2074f7cd7a3d7d3
aaaeka <36422478+aaaeka@users.noreply.github.com>
2025-03-24 11:00:12 +0200

    Avante setup

--- init.lua
+++ init.lua
@@ -754,6 +754,8 @@ require('lazy').setup({
 -- which-key group setup
 local wk = require 'which-key'
 wk.add {
+  { '<leader>a', group = '[A]vante' },
+  { '<leader>a_', hidden = true },
   { '<leader>c', group = '[C]ode' },
   { '<leader>c_', hidden = true },
   { '<leader>d', group = '[D]ocument' },
