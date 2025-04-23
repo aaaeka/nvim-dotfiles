ffc7a7acd28d51086ddee654ae7baf93c15ec178
aaaeka <36422478+aaaeka@users.noreply.github.com>
2025-04-23 10:56:27 +0300

    Add gemini avante config

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -4,9 +4,7 @@ return {
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
   opts = {
-    -- add any opts here
-    -- for example
-    provider = 'openai',
+    provider = 'gemini',
     hints = {
       enabled = false,
     },
@@ -17,6 +15,13 @@ return {
       temperature = 0,
       max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
       reasoning_effort = 'medium', -- low|medium|high, only used for reasoning models
+    },
+    gemini = {
+      endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
+      model = 'gemini-2.5-pro-exp-03-25',
+      timeout = 30000,
+      temperature = 1,
+      max_tokens = 8192,
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
